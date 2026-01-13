52f0f59b9d14c35a97d357fa315f8496abaced26
zivglazer <glazerz@post.bgu.ac.il>
2026-01-13 17:38:19 +0200

    new

--- Assignment 3 SPL Skeleton/Assignment 3 SPL/server/src/main/java/bgu/spl/net/srv/BlockingConnectionHandler.java
+++ Assignment 3 SPL Skeleton/Assignment 3 SPL/server/src/main/java/bgu/spl/net/srv/BlockingConnectionHandler.java
@@ -1,7 +1,8 @@
 package bgu.spl.net.srv;
 
 import bgu.spl.net.api.MessageEncoderDecoder;
-import bgu.spl.net.api.MessagingProtocol;
+import bgu.spl.net.api.StompMessagingProtocol;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
@@ -9,52 +10,73 @@ import java.net.Socket;
 
 public class BlockingConnectionHandler<T> implements Runnable, ConnectionHandler<T> {
 
-    private final MessagingProtocol<T> protocol;
+    private final StompMessagingProtocol<T> protocol;
     private final MessageEncoderDecoder<T> encdec;
     private final Socket sock;
+    private final Connections<T> connections;
+    private final int connectionId;
+
     private BufferedInputStream in;
     private BufferedOutputStream out;
+
+    private final Object writeLock = new Object();
     private volatile boolean connected = true;
 
-    public BlockingConnectionHandler(Socket sock, MessageEncoderDecoder<T> reader, MessagingProtocol<T> protocol) {
+    public BlockingConnectionHandler(Socket sock,
+                                    MessageEncoderDecoder<T> reader,
+                                    StompMessagingProtocol<T> protocol,
+                                    Connections<T> connections,
+                                    int connectionId) {
         this.sock = sock;
         this.encdec = reader;
         this.protocol = protocol;
+        this.connections = connections;
+        this.connectionId = connectionId;
     }
 
     @Override
     public void run() {
-        try (Socket sock = this.sock) { //just for automatic closing
-            int read;
-
+        try (Socket ignored = this.sock) {
             in = new BufferedInputStream(sock.getInputStream());
             out = new BufferedOutputStream(sock.getOutputStream());
 
-            while (!protocol.shouldTerminate() && connected && (read = in.read()) >= 0) {
+            protocol.start(connectionId, connections);
+
+            int read;
+            while (connected && !protocol.shouldTerminate() && (read = in.read()) >= 0) {
                 T nextMessage = encdec.decodeNextByte((byte) read);
                 if (nextMessage != null) {
-                    T response = protocol.process(nextMessage);
-                    if (response != null) {
-                        out.write(encdec.encode(response));
-                        out.flush();
-                    }
+      
+                    protocol.process(nextMessage);
                 }
             }
-
         } catch (IOException ex) {
-            ex.printStackTrace();
+         
+        } finally {
+            connected = false;
+            connections.disconnect(connectionId);
         }
-
     }
-
     @Override
     public void close() throws IOException {
         connected = false;
         sock.close();
     }
-
     @Override
     public void send(T msg) {
-        //IMPLEMENT IF NEEDED
+        if (msg == null) return;
+
+        byte[] bytes = encdec.encode(msg);
+
+        synchronized (writeLock) {
+            if (!connected || out == null) return;
+            try {
+                out.write(bytes);
+                out.flush();
+            } catch (IOException e) {
+                connected = false;
+                connections.disconnect(connectionId);
+            }
+        }
     }
 }
